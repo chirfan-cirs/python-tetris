d6ebfb2f8f541245a908bcbc26ff8f188fd565a1
chirfan1 <44518884+chirfan1@users.noreply.github.com>
2023-08-22 16:02:07 +0700

    Color, Shape, Logic

--- tetris.py
+++ tetris.py
@@ -1,1 +1,183 @@
-print("Hallo")
+import sys
+import pygame
+import random
+
+pygame.init()
+
+#Screen dimensions
+WIDTH, HEIGHT = 800, 600
+GRID_SIZE = 25
+
+#Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+COLORS = [RED, BLUE, GREEN]
+
+#Tetromino shapes
+SHAPES = [
+    #Blok
+    [
+        ['.....',
+         '.....',
+         '.....',
+         '0000.',
+         '.....'],
+        ['.....',
+         '..0..',
+         '..0..',
+         '..0..',
+         '..0..']
+    ],
+    #4 Blok
+    [
+        ['.....',
+         '.....',
+         '..0..',
+         '.000.',
+         '.....'],
+        ['.....',
+         '..0..',
+         '.00..',
+         '..0..',
+         '.....'],
+        ['.....',
+         '.....',
+         '.000.',
+         '..0..',
+         '.....'],
+        ['.....',
+         '..0..',
+         '..00.',
+         '..0..',
+         '.....'],
+    ],
+    #Trapesium
+    [
+        ['.....',
+         '.....',
+         '..00.',
+         '.00..',
+         '.....'],
+        ['.....',
+         '.....',
+         '.00..',
+         '..00.',
+         '.....'],
+        ['.....',
+         '.0...',
+         '.00..',
+         '..0..',
+         '.....'],
+        ['.....',
+         '..0..',
+         '.00..',
+         '.0...',
+         '.....']
+    ],
+    #Blok L
+    [
+        ['.....',
+         '..0..',
+         '..0..',
+         '..00.',
+         '.....'],
+        ['.....',
+         '...0.',
+         '.000.',
+         '.....',
+         '.....'],
+        ['.....',
+         '.00..',
+         '..0..',
+         '..0..',
+         '.....'],
+        ['.....',
+         '.....',
+         '.000.',
+         '.0...',
+         '.....'],
+    ],
+]
+
+#Classes
+class Tetromino:
+    def __init__(self, x, y, shape):
+        self.x = x
+        self.y = y
+        self.shape = shape
+        self.color = random.choice(COLORS)
+        self.rotation = 0
+
+
+class Tetris:
+    def __init__(self, width, heiht):
+        self.width = width
+        self.heiht = heiht
+        self.grid = [[0 for _ in range(width)] for _ in range(heiht)]
+        self.current_piece = self.new_piece()
+        self.game_over = False
+        self.score = 0
+
+
+    def new_piece(self):
+        shape = random.choice(SHAPES)
+        return Tetromino(self.width // 2, 0, shape)
+
+    def valid_move(self, piece, x, y, rotation):
+        for i, row in enumerate(piece.shape[(piece.rotation + rotation) % len(piece.shape)]):
+            for j, cell in enumerate(row):
+                try:
+                    if cell == '0' and (self.grid[piece.y + i + y][piece.x + j + x] != 0):
+                        return False
+                except IndexError:
+                    return False
+        return True
+
+    def clear_lines(self):
+        lines_cleared = 0
+        for i, row in enumerate(self.grid[:-1]):
+            if all(cell != 0 for cell in row):
+                lines_cleared += 1
+                del self.grid[i]
+                self.grid.insert(0, [0 for _ in range(self.width)])
+        return lines_cleared
+
+    def lock_piece(self, piece):
+        for i, row in enumerate(piece.shape[piece.rotation % len(piece.shape)]):
+            for j, cell in enumerate(row):
+                if cell == '0':
+                    self.grid[piece.y + i][piece.x + j ] = piece.color
+
+            lines_cleared = self.clear_lines()
+            self.score += lines_cleared * 100
+            self.current_piece = self.new_piece()
+            if not self.valid_move(self.current_piece, 0, 0, 0):
+                self.game_over = True
+            return lines_cleared
+
+
+    def update(self):
+        if not self.game_over:
+            if self.valid_move(self.current_piece, 0, 1, 0):
+                self.current_piece.y += 1
+            else:
+                self.lock_piece(self.current_piece)
+
+
+    def draw(self,screen):
+        for y, row in enumerate(self.grid):
+            for x, cell in enumerate(row):
+                if cell:
+                    pygame.draw.rect(screen, cell, (x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE - 1, GRID_SIZE - 1))
+
+
+        if self.current_piece:
+            for i, row in enumerate(self.current_piece.shape[self.current_piece.rotation % len(self.current_piece.shape)]):
+                for j, cell in enumerate(row):
+                    if cell == '0':
+                        pygame.draw.rect((screen, self.current_piece.color, ((self.current_piece.x + j) * GRID_SIZE, (self.current_piece.y + i) * GRID_SIZE, GRID_SIZE - 1, GRID_SIZE - 1)))
+
+
