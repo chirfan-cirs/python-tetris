900431a90b22114feababac69406f4805cd94ee5
chirfan1 <44518884+chirfan1@users.noreply.github.com>
2023-08-23 16:24:47 +0700

    Fix rotation and clear line

--- tetris.py
+++ tetris.py
@@ -5,7 +5,7 @@ import random
 pygame.init()
 
 # Screen dimensions
-WIDTH, HEIGHT = 450, 650
+WIDTH, HEIGHT = 450, 600
 GRID_SIZE = 25
 
 # Colors
@@ -40,7 +40,7 @@ SHAPES = [
          '.....'],
         ['.....',
          '..0..',
-         '.00..',
+         '..00.',
          '..0..',
          '.....'],
         ['.....',
@@ -50,7 +50,7 @@ SHAPES = [
          '.....'],
         ['.....',
          '..0..',
-         '..00.',
+         '.00..',
          '..0..',
          '.....'],
     ],
@@ -82,36 +82,31 @@ SHAPES = [
     # Blok L
     [
         ['.....',
-         '..0..',
-         '..0..',
-         '..00.',
-         '.....'],
-        ['.....',
          '...0.',
          '.000.',
          '.....',
          '.....'],
         ['.....',
-         '.00..',
          '..0..',
          '..0..',
+         '..00.',
          '.....'],
         ['.....',
          '.....',
          '.000.',
          '.0...',
+         '.....'],
+        ['.....',
+         '.00..',
+         '..0..',
+         '..0..',
          '.....'],
     ],
     [
         ['.....',
-         '..0..',
-         '..0..',
-         '.00..',
-         '.....'],
-        ['.....',
-         '.....',
+         '.0...',
          '.000.',
-         '...0.',
+         '.....',
          '.....'],
         ['.....',
          '..00.',
@@ -119,9 +114,14 @@ SHAPES = [
          '..0..',
          '.....'],
         ['.....',
-         '.0...',
-         '.000.',
          '.....',
+         '.000.',
+         '...0.',
+         '.....'],
+        ['.....',
+         '..0..',
+         '..0..',
+         '.00..',
          '.....'],
     ],
 ]
@@ -150,6 +150,7 @@ class Tetris:
         return Tetromino(self.width // 2, 0, shape)
 
     def valid_move(self, piece, x, y, rotation):
+        """Check if the piece can move to the given position"""
         for i, row in enumerate(piece.shape[(piece.rotation + rotation) % len(piece.shape)]):
             for j, cell in enumerate(row):
                 try:
@@ -160,6 +161,7 @@ class Tetris:
         return True
 
     def clear_lines(self):
+        """Clear the lines that are full and return the number of cleared lines"""
         lines_cleared = 0
         for i, row in enumerate(self.grid[:-1]):
             if all(cell != 0 for cell in row):
@@ -169,6 +171,7 @@ class Tetris:
         return lines_cleared
 
     def lock_piece(self, piece):
+        """Lock the piece in place and create a new piece"""
         for i, row in enumerate(piece.shape[piece.rotation % len(piece.shape)]):
             for j, cell in enumerate(row):
                 if cell == '0':
@@ -181,46 +184,53 @@ class Tetris:
             self.game_over = True
         return lines_cleared
 
-
     def update(self):
+        """Make the piece down, one by one"""
         if not self.game_over:
             if self.valid_move(self.current_piece, 0, 1, 0):
                 self.current_piece.y += 1
             else:
                 self.lock_piece(self.current_piece)
 
-    def draw(self,screen):
+    def draw(self, screen):
+        """Draw the grid and the current piece"""
         for y, row in enumerate(self.grid):
             for x, cell in enumerate(row):
                 if cell:
                     pygame.draw.rect(screen, cell, (x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE - 1, GRID_SIZE - 1))
 
         if self.current_piece:
-            for i, row in enumerate(self.current_piece.shape[self.current_piece.rotation % len(self.current_piece.shape)]):
+            for i, row in enumerate(
+                    self.current_piece.shape[self.current_piece.rotation % len(self.current_piece.shape)]):
                 for j, cell in enumerate(row):
                     if cell == '0':
-                        pygame.draw.rect(screen, self.current_piece.color, ((self.current_piece.x + j) * GRID_SIZE, (self.current_piece.y + i) * GRID_SIZE, GRID_SIZE - 1, GRID_SIZE - 1))
+                        pygame.draw.rect(screen, self.current_piece.color, (
+                        (self.current_piece.x + j) * GRID_SIZE, (self.current_piece.y + i) * GRID_SIZE, GRID_SIZE - 1,
+                        GRID_SIZE - 1))
 
 
 def draw_score(screen, score, x, y):
+    """Draw the score on the screen"""
     font = pygame.font.Font(None, 36)
-    text = font.render(f"Score : {score}", True, WHITE)
+    text = font.render(f"Score: {score}", True, WHITE)
     screen.blit(text, (x, y))
 
 
 def draw_game_over(screen, x, y):
+    """Draw the game over text on the screen"""
     font = pygame.font.Font(None, 48)
     text = font.render("Game Over", True, RED)
     screen.blit(text, (x, y))
 
 
 def main():
+    # Initialize pygame
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption('Tetris')
     clock = pygame.time.Clock()
     game = Tetris(WIDTH // GRID_SIZE, HEIGHT // GRID_SIZE)
-    fall_time = 0
-    fall_speed = 75
+    fall_time = 10
+    fall_speed = 150
 
     while True:
         screen.fill(BLACK)
@@ -230,7 +240,7 @@ def main():
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    if game.valid_move(game.current_piece, -1, 0, 0) and game.current_piece.x > 0:
+                    if game.valid_move(game.current_piece, -1, 0, 0):
                         game.current_piece.x -= 1
                 if event.key == pygame.K_RIGHT:
                     if game.valid_move(game.current_piece, 1, 0, 0):
@@ -245,20 +255,17 @@ def main():
                     while game.valid_move(game.current_piece, 0, 1, 0):
                         game.current_piece.y += 1
                     game.lock_piece(game.current_piece)
-
         delta_time = clock.get_rawtime()
         fall_time += delta_time
         if fall_time >= fall_speed:
             game.update()
             fall_time = 0
-
         draw_score(screen, game.score, 10, 10)
         game.draw(screen)
         if game.game_over:
             draw_game_over(screen, WIDTH // 2 - 100, HEIGHT // 2 - 30)
             if event.type == pygame.KEYDOWN:
                 game = Tetris(WIDTH // GRID_SIZE, HEIGHT // GRID_SIZE)
-
         pygame.display.flip()
         clock.tick(60)
 
